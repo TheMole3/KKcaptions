904e1cafc9eeeeb60374ca89419bccdc3001c51b
TheMole3 <hugomelo04@gmail.com>
2021-04-22 15:57:35 +0200

    Updating to refractor

--- public/components/colors.js
+++
@@ -1,9 +0,0 @@
-var colors = {
-    richBlackFogra: "#071013",
-    lavenderWeb: "#eeeeff",
-    sonicSilver: "#707078",
-    etonBlue: "#79c99e",
-    tartOrange: "#f0544f",
-}
-  
-export default colors;
